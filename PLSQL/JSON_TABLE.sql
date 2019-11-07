6d4c514bb9239fd37476c5114334171b28007b14
Asfakul <asfkol@gmail.com>
2019-11-08 00:13:48 +0530

    Initial

---
+++ PLSQL/JSON_TABLE.sql
@@ -0,0 +1,47 @@
+CREATE TABLE j_purchaseorder
+  (id RAW (16) NOT NULL,
+   date_loaded TIMESTAMP(6) WITH TIME ZONE,
+   po_document CLOB CONSTRAINT ensure_json CHECK (po_document IS JSON));
+
+INSERT INTO j_purchaseorder
+  VALUES (
+    SYS_GUID(),
+    SYSTIMESTAMP,
+    '{"PONumber"              : 1600,
+      "Reference"             : "ABULL-20140421",
+       "Requestor"            : "Alexis Bull",
+       "User"                 : "ABULL",
+       "CostCenter"           : "A50",
+       "ShippingInstructions" : {"name"   : "Alexis Bull",
+                                 "Address": {"street"   : "200 Sporting Green",
+                                              "city"    : "South San Francisco",
+                                              "state"   : "CA",
+                                              "zipCode" : 99236,
+                                              "country" : "United States of America"},
+                                 "Phone" : [{"type" : "Office", "number" : "909-555-7307"},
+                                            {"type" : "Mobile", "number" : "415-555-1234"}]},
+       "Special Instructions" : null,
+       "AllowPartialShipment" : true,
+       "LineItems" : [{"ItemNumber" : 1,
+                       "Part" : {"Description" : "One Magic Christmas",
+                                 "UnitPrice"   : 19.95,
+                                 "UPCCode"     : 13131092899},
+                       "Quantity" : 9.0},
+                      {"ItemNumber" : 2,
+                       "Part" : {"Description" : "Lethal Weapon",
+                                 "UnitPrice"   : 19.95,
+                                 "UPCCode"     : 85391628927},
+                       "Quantity" : 5.0}]}');
+
+
+select jt.phones
+from j_purchaseorder,
+JSON_TABLE(po_document, '$.ShippingInstructions'
+COLUMNS 
+    (phones VARCHAR2(1000) FORMAT JSON PATH '$.Phone' )) AS jt;
+
+SELECT jt.phones
+FROM j_purchaseorder,
+JSON_TABLE(po_document, '$.ShippingInstructions'
+COLUMNS
+  (phones VARCHAR2(100) FORMAT JSON PATH '$.Phone')) AS jt;
--- PLSQL/ords.sql
+++ PLSQL/ords.sql
@@ -21,17 +21,17 @@ begin
 ORDS.define_service(
 p_module_name => 'rest-v1',
 p_base_path => 'rest-v1/',
-p_pattern   => 'employees/',
+p_pattern   => 'employees/:empno',
 p_method    => 'GET',
 p_source_type   => ORDS.source_type_collection_feed,
-p_source    => 'select * from employees',
+p_source    => 'select * from employees where employee_id = :empno',
 p_items_per_page    => 0
 );
 COMMIT;
 
 end;
 /
-
+--common views for ords
 SELECT id, name, uri_prefix
 FROM   user_ords_modules
 ORDER BY name;
@@ -44,3 +44,158 @@ ORDER BY module_id;
 SELECT id, template_id, source_type, method, source
 FROM   user_ords_handlers
 ORDER BY id;
+
+---manually creating the above process
+
+BEGIN
+ORDS.define_module(
+p_module_name => 'rest-v2',
+p_base_path => 'rest-v2/',
+p_items_per_page => 0
+);
+ORDS.define_template(
+p_module_name => 'rest-v2',
+p_pattern => 'employees/'
+);
+
+ORDS.define_handler(
+p_module_name => 'rest-v2',
+p_pattern => 'employees/',
+p_method => 'GET',
+p_source_type => ORDS.source_type_collection_feed,
+p_source => 'select * from employees',
+p_items_per_page => 0
+);
+
+ORDS.define_template(
+p_module_name => 'rest-v2',
+p_pattern => 'employees/:empno' --Handlers is associated with patterns in template ***
+);
+
+ORDS.define_handler(
+p_module_name => 'rest-v2',
+p_pattern => 'employees/:empno',
+p_method => 'GET',
+p_source_type => ORDS.source_type_collection_feed,
+p_source => 'select * from employees where employee_id = :empno',
+p_items_per_page => 0
+);
+commit ; -- it's absolutely necessary 
+
+END;
+/
+
+--Multi value parameters
+select instr('1',',') from dual;
+
+create type emp_t is table of varchar2(40);
+
+/
+create or replace function in_list(p_in_list varchar2)
+return emp_t pipelined
+as
+l_in_list VARCHAR2(250) := p_in_list||',';
+l_id NUMBER := 1;
+begin
+loop
+
+    l_id := INSTR(l_in_list,',');
+    exit when l_id =0;
+    dbms_output.put_line(l_id);
+    pipe row (TRIM(substr(l_in_list,1,l_id-1)));
+    l_in_list:=NVL(SUBSTR(l_in_list,l_id+1),' ');
+    dbms_output.put_line(l_in_list);
+    
+end loop;
+
+end;
+/
+select * from in_list('1,2,4,65,7');
+/
+
+begin
+ORDS.define_module(
+p_module_name => 'rest-v3',
+p_base_path => 'rest-v3/'
+);
+
+ORDS.define_template(
+p_module_name => 'rest-v3',
+p_pattern => 'employees/:empno'
+);
+
+ORDS.define_handler(
+p_module_name => 'rest-v3',
+p_pattern => 'employees/:empno',
+p_source_type => ORDS.source_type_collection_feed,
+p_source => 'select * from employees where employee_id in (select * from in_list(:empno))'
+);
+COMMIT;
+
+end;
+/
+--call PLSQL procedure
+
+CREATE OR REPLACE PROCEDURE get_emp_json(p_empno IN employees.employee_id%type default NULL)
+as
+l_cursor SYS_REFCURSOR;
+BEGIN
+open l_cursor for
+select  e.employee_id as "empno",
+        e.first_name|| ''||e.last_name as "ename",
+        e.salary as "sal"
+from employees e 
+where employee_id = NVL(p_empno,employee_id);
+APEX_JSON.open_object;
+APEX_JSON.write('employees',l_cursor);
+APEX_JSON.close_object;
+
+END;
+/
+
+begin
+ORDS.define_module(
+p_module_name => 'rest-v4',
+p_base_path => 'rest-v4/'
+);
+
+ORDS.define_template(
+p_module_name => 'rest-v4',
+p_pattern => 'employees/:empno'
+);
+
+ORDS.define_handler(
+p_module_name => 'rest-v4',
+p_pattern => 'employees/:empno',
+p_source_type => ORDS.source_type_plsql,
+p_source => 'begin get_emp_json(:empno); end;'
+);
+
+
+ORDS.define_template(
+p_module_name => 'rest-v4',
+p_pattern => 'employees/'
+);
+
+ORDS.define_handler(
+p_module_name => 'rest-v4',
+p_pattern => 'employees/',
+p_source_type => ORDS.source_type_plsql,
+p_source => 'begin get_emp_json(); end;'
+);
+
+
+COMMIT;
+
+
+end;
+/
+
+--POST Method
+
+-- http://localhost:9090/ords/hr
+
+
+
+
+
